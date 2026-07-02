c998b3ecbd75bbd868ba42bf884e26db2311ea4c
Felipe Fernandes Morotti <felipe-morotti@outlook.com>
2026-07-02 10:35:30 -0300

    ch5

---
+++ ch4-bookstore/pages/views.py
@@ -0,0 +1,8 @@
+# Create your views here.
+from django.shortcuts import render
+
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
